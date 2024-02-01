c1ed1966aec981bee39d29f2134d822a24d280bc
BrianBontempi <brian.bont54@gmail.com>
2024-02-01 19:12:16 +0100

    hello vite =)

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+    name: 'HelloVite',
+
+    components: {
+        AppTitle,
+    }
+};
+</script>
+
+<template>
+    <div class="container">
+        <AppTitle />
+    </div>
+</template>
